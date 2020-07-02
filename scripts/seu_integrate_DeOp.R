96da1c14c9748eeb84dbf119ccc6df2dd8cd0dc6
Johanna GALVIS <johanna.galvis@inmg686.univ-lyon1.fr>
2020-07-02 15:59:53 +0200

    Oprescu and DeMicheli integrated, pending impute cell types

---
+++ scripts/seu_integrate_DeOp.R
@@ -0,0 +1,133 @@
+# INTEGRATED ANALYSIS  ** Oprescu and DeMicheli **
+# Integrating Oprescu and DeMicheli D0 data. This is the main integration job, 
+# reason: both on tibialis anterior m, similar experimental design
+
+# --
+# JohaGL
+
+library(dplyr)
+library(Seurat)
+library(ggplot2)
+library(sctransform)
+library(cowplot)
+library(RColorBrewer)
+source(file="~/INMG_SingleCell/scripts/functions_stock.R",local=T)
+
+prloc="~/INMG_SingleCell/"
+setwd(prloc)
+resu="results/integratedD0_DeOp/"
+rdsdir = "rds/integratedD0_DeOp/"
+dir.create(resu,recursive=T)
+dir.create(rdsdir, recursive=T)
+nbDIM=15
+
+print("Integrating Oprescu and DeMicheli D0 data. This is the main integration job, 
+  reason: both on tibialis anterior m, similar experimental design")
+print(paste0("Will use *",nbDIM,"* princ components (dims), accordingly to  ", 
+             "elbowplot and good balance identities-nbclusters seen in independent runs"))
+
+opre.data <- read.table("data/OprescuD0/oprescu_Noninjured_raw.txt", sep="\t",
+                        header=T, row.names = 1)
+opre <- CreateSeuratObject(opre.data, project="Oprescu", min.cells=3, min.features=200)
+
+dmich.data <- read.table("data/DeMicheliD0/rawdataD0.txt", sep="\t",
+                    header=T, row.names=1)
+dmich <- CreateSeuratObject(dmich.data, project="DeMicheli", min.cells=3,min.features = 200)
+
+# ====================================================================================
+print("adding metadata concerning origin of datasets")
+opre@meta.data[["orig.ident"]] <- rep("Oprescu", length( opre@meta.data[["orig.ident"]] ))
+dmich@meta.data[["orig.ident"]] <- rep("DeMicheli", length( dmich@meta.data[["orig.ident"]] ))
+# ====================================================================================
+print("filter mitochondrial genes and low quality cells")
+opre [["percent.mt"]] <- PercentageFeatureSet(opre, pattern= "^mt-")
+dmich[["percent.mt"]] <- PercentageFeatureSet(dmich, pattern= "^mt-")
+
+maxUMIop = 35000
+maxUMIde = 25000
+permit = 10
+print(paste0("doing data filtering, admit max nbUMI: ", maxUMIop, " oprescu,  ",
+             maxUMIde," demicheli. For both, admited mitochondrial % ",permit))
+opre <- subset(opre, subset=nFeature_RNA > 200 & nCount_RNA <= maxUMIop & percent.mt < permit)
+dmich <- subset(dmich, subset=nFeature_RNA > 200 & nCount_RNA <= maxUMIde & percent.mt < permit)
+
+
+# ====================================================================================
+# integration
+newfeat.intgr = 6000
+print(paste0("integrating, using nbfeatures= ",newfeat.integr)
+
+muscle.list <- c(opre,dmich)
+# preprocessing: log-normalisation and variable features
+for (i in 1:length(x=muscle.list)) {
+  muscle.list[[i]] <- NormalizeData(muscle.list[[i]],verbose=FALSE)
+  muscle.list[[i]] <- FindVariableFeatures(muscle.list[[i]], 
+                                           selection.method="vst",
+                                           nfeatures= newfeat.intgr, verbose=FALSE)
+}
+muscle.anchors <- FindIntegrationAnchors(object.list = muscle.list, dims=1:30)
+muscle.integrated <- IntegrateData(anchorset = muscle.anchors, dims = 1:30)
+
+# switch to integrated assay. The variable features of this assay are
+# automatically set during IntegrateData
+DefaultAssay(object = muscle.integrated) <- "integrated"
+
+# =====================================================================================
+# find clusters
+# =====================================================================================
+print("scaling, reducing dim (PCA), printing ElbowPlot into preprocessIntegrated.pdf")
+muscle.integrated <- ScaleData(muscle.integrated, verbose = FALSE)
+muscle.integrated <- RunPCA(muscle.integrated, npcs = 30, verbose = FALSE)
+#visuals
+pdf(paste0(resu,"preprocessIntegrated.pdf"))
+ElbowPlot(muscle.integrated)
+DimHeatmap(muscle.integrated, dims=1:30, cells = 500, balanced = TRUE)
+dev.off()
+
+muscle.integrated <- KNNplusFITSNE(muscle.integrated, nbDIM, 0.4)
+saveRDS(muscle.integrated,file=paste0(rdsdir,"integrated_seu_fitsne.rds"))
+
+# =====================================================================================
+# markers : both, positive and negative LFC
+# =====================================================================================
+muscle.integrated.markers <- FindAllMarkers(muscle.integrated, only.pos = FALSE, 
+                                            min.pct = 0.25, logfc.threshold = 0.25) #time consuming
+# save into two tables:
+threshold = 0.5 #threshold for log2foldChange
+positive <- muscle.integrated.markers %>% filter(avg_logFC > threshold)
+negative <- muscle.integrated.markers %>% filter(avg_logFC < -threshold)
+
+write.table(positive, paste0(resu,"ALLMARKERS_Pos_integratedD0.txt"), sep="\t")
+write.table(negative, paste0(resu,"ALLMARKERS_Neg_integratedD0.txt"), sep="\t")
+
+# example getting 8 most relevant genes Pos and Neg :
+most.neg <- negative %>% group_by(cluster) %>% top_n(n=4, wt= -avg_logFC)
+most.pos <- positive %>% group_by(cluster) %>% top_n(n=4, wt= avg_logFC)
+
+# =====================================================================================
+# preliminary plots into results
+# =====================================================================================
+
+pdf(paste0(resu,"FITSNE.pdf"))
+DimPlot(muscle.integrated, reduction="tsne", label=T,
+        cols=definecolors(muscle.integrated@active.ident)) + ggtitle("Integrated D0")
+dev.off()
+pos4top <- positive %>% group_by(cluster) %>% top_n(n=4, wt= avg_logFC)
+pdf(paste0(resu,"HEATMAP.pdf"), width=13)
+DoHeatmap(muscle.integrated, features = pos4top$gene, 
+          group.colors=definecolors(muscle.integrated@active.ident)) 
+dev.off()
+
+print("finished")
+
+
+# ====================================================================================
+# 
+sessionInfo()
+
+ctrans <- c( rgb(0.15, 0.34, 0.95, alpha=0.4),
+             rgb(0.4, 0.77, 0.50, alpha=0.5))
+DimPlot(object = muscle.integrated, cols=ctrans, reduction = "tsne",
+        group.by = "orig.ident")
+
+
