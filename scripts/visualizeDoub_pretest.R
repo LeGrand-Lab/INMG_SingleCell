87ca13e7cf21e8b35a3365efe51495b9ad860035
johanna <johanna@localhost.localdomain>
2020-07-10 11:20:42 +0200

    comparing methods doublets

--- scripts/visualizeDoub_pretest.R
+++ scripts/visualizeDoub_pretest.R
@@ -4,6 +4,8 @@ library(Seurat)
 library(scran)
 library(scater)
 library(SingleCellExperiment)
+library(RColorBrewer)
+library(cowplot)
 
 setwd("~/INMG_SingleCell/")
 resu = "results/integratedD0/DCscran/"
@@ -60,4 +62,47 @@ head(seu@reductions[["tsne"]]@cell.embeddings)
 typeof(sce@int_colData@listData[["reducedDims"]]@listData[["TSNE"]])
 typeof(seu@reductions[["tsne"]]@cell.embeddings)
 
+
+# quqntiles:
+head(as.numeric(as.character(seu@meta.data$doublet_score)))
+x <- as.numeric(as.character(seu@meta.data$doublet_score))
+
+quantile(x,0.995)
+# 99.5% 
+# 4.942606 
+quantile(x,0.95)
+# 95% 
+# 3.583153 
+
+bicho = as.data.frame(colData(sce))
+bicho <-  bicho %>% mutate(doubinterval = case_when(
+  doublet_score <= 3.5 ~ "1. <= 3.5",
+  doublet_score > 3.5 & doublet_score < 5 ~ "2. 3.5 - 5",
+  TRUE ~ "3. >= 5" # doublet_score > 4.5
+))
+colData(sce)$doubinterval = as.factor(bicho$doubinterval)
+
+# visualization not very good:
+a <- plotReducedDim(sce, dimred="FItSNE", colour_by = "celltype")
+b <- plotReducedDim(sce, dimred="FItSNE", colour_by = "doulet_score")
+c <- plotReducedDim(sce, dimred= "FItSNE", colour_by = "doubinterval")
+
+thecols = c(rgb(0.9, 0.2, 0.3, 1),
+            rgb(0.2, 0.5, 0.8, 0.5), 
+            rgb(0.1, 0.7, 0.3, 0.1))
+
+seu@meta.data$doublet_score = as.factor(colData(sce)$doublet_score)
+seu@meta.data$doubinterval = as.factor(colData(sce)$doubinterval)
+levels(seu@meta.data$doubinterval) = c( "<= 3.5", "3.5 - 5", ">= 5" )
+b <- plotReducedDim(sce, dimred="FItSNE", colour_by = "doublet_score")
+l <- qplot(colData(sce)$doublet_score)
+m <- DimPlot(seu, group.by = "doubinterval", cols=rev(thecols))
+n <- DimPlot(seu, group.by = "celltype", cols=
+               colorRampPalette(brewer.pal(8,"Set2"))(length(levels(seu$celltype))),
+             label=T, repel=T, size=2)+NoLegend()
+
+
+pdf(paste0(resu,"scranMeth.pdf"))
+plot_grid(m,n,l,b, ncol=2)
+dev.off()
 
